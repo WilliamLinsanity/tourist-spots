e0df7f6cada8bc6ae784a0dbb2a1fc1b2f80faf1
WilliamLinsanity <s3211t@gmail.com>
2021-11-13 04:32:48 +0800

    main frame second version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.scss';
 import Header from './Header'
 import MainFrame from './MainFrame';
+
 function App() {
-  // fetch('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot').then(res=>res.json()).then(res=>{
-  //   console.log(res);
-  // })
   return (
     <div className="App">
-     <Header/>
+     <Header />
      <MainFrame/>
     </div>
   );
